dd4ed0d94bb3862b6082568b873981af92755e88
nagichev <nagich.ev@yandex.ru>
2024-12-10 18:53:55 +0300

    Initial commit

---
+++ src/main/java/koschei/models/Death8.java
@@ -0,0 +1,4 @@
+package koschei.models;
+
+public class Death8 {
+}
---
+++ src/main/java/koschei/models/Duck5.java
@@ -0,0 +1,18 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Duck5 {
+
+    @Autowired
+    private Egg6 egg;
+
+    public Duck5(Egg6 egg) {
+        this.egg = egg;
+    }
+
+    @Override
+    public String toString() {
+        return ", в утке яйцо " + egg.toString();
+    }
+}
